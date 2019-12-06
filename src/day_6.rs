6d387d3a5abd90762e68b769ffa2df4d71cb9313
Petr Volf <Petr.volf216@gmail.com>
2019-12-06 12:19:05 +0100

    day 6 part 2

--- src/day_6.rs
+++ src/day_6.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
+use std::usize;
 
 pub fn solve() -> Result<(), Box<dyn Error>> {
     let input_file = File::open("./src/input_6")?;
@@ -34,6 +35,8 @@ pub fn solve() -> Result<(), Box<dyn Error>> {
 
     dbg!(orbits.len() + indir_orbits);
 
+    find_nca("SAN", "YOU", &orbits);
+
     Ok(())
 }
 
@@ -44,6 +47,41 @@ fn search_for_parent<'a>(orbits: &'a [Orbit], child: &String) -> Option<&'a Stri
         }
     }
     None
+}
+
+fn find_nca(first: &str, second: &str, orbits: &[Orbit]) {
+    let first_tree = find_all_ancestors(first, orbits);
+    let second_tree = find_all_ancestors(second, orbits);
+    let mut min_jump  = usize::MAX;
+    for (index_first, first) in first_tree.iter().enumerate() {
+        for (index_second, second) in second_tree.iter().enumerate() {
+            if first == second {
+                if index_first + index_second < min_jump {
+                    min_jump = index_first + index_second;
+                }
+            }
+        }
+    }
+    dbg!(min_jump);
+}
+
+fn find_all_ancestors<'a>(orb: &str, orbits: &'a [Orbit]) -> Vec<&'a String> {
+    let mut vec = Vec::new();
+    for orbit in orbits {
+        if orbit.name.as_str() != orb {
+            continue;
+        }
+        let mut current_parent = &orbit.parent;
+        vec.push(current_parent);
+        loop {
+            if current_parent.as_str() == "COM" {
+                break;
+            }
+            current_parent = search_for_parent(&orbits, current_parent).unwrap();
+            vec.push(current_parent);
+        }
+    }
+    vec
 }
 
 #[derive(Debug, Clone, PartialEq)]
